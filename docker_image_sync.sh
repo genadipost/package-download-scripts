a72afde089cfdddd26bdc8c774ae6dd2d0ce7bd0
genadipost <genadipost@gmail.com>
2017-10-14 11:49:50 +0300

    add -a flag to registry.access.redhat images

--- docker_image_sync.sh
+++ docker_image_sync.sh
@@ -1,23 +1,23 @@
-docker search registry.access.redhat.com/rhscl/* --limit 100 | cut -d" " -f 4 | xargs -L 1 docker pull
-docker search registry.access.redhat.com/openshift3/* --limit 100 | cut -d" " -f 4 | xargs -L 1 docker pull
-docker search registry.access.redhat.com/rhgs3/* --limit 100 | cut -d" " -f 4 | xargs -L 1 docker pull
-docker search registry.access.redhat.com/redhat-sso-7/* --limit 100 | cut -d" " -f 4 | xargs -L 1 docker pull
-docker search registry.access.redhat.com/jboss-eap-6/* --limit 100 | cut -d" " -f 4 | xargs -L 1 docker pull
-docker search registry.access.redhat.com/jboss-eap-7/* --limit 100 | cut -d" " -f 4 | xargs -L 1 docker pull
-docker search registry.access.redhat.com/jboss-webserver-3/* --limit 100 | cut -d" " -f 4 | xargs -L 1 docker pull
-docker search registry.access.redhat.com/jboss-datagrid-6/* --limit 100 | cut -d" " -f 4 | xargs -L 1 docker pull
+docker search registry.access.redhat.com/rhscl/* --limit 100 | cut -d" " -f 4 | xargs -L 1 docker pull -a
+docker search registry.access.redhat.com/openshift3/* --limit 100 | cut -d" " -f 4 | xargs -L 1 docker pull -a 
+docker search registry.access.redhat.com/rhgs3/* --limit 100 | cut -d" " -f 4 | xargs -L 1 docker pull -a 
+docker search registry.access.redhat.com/redhat-sso-7/* --limit 100 | cut -d" " -f 4 | xargs -L 1 docker pull -a 
+docker search registry.access.redhat.com/jboss-eap-6/* --limit 100 | cut -d" " -f 4 | xargs -L 1 docker pull -a 
+docker search registry.access.redhat.com/jboss-eap-7/* --limit 100 | cut -d" " -f 4 | xargs -L 1 docker pull -a 
+docker search registry.access.redhat.com/jboss-webserver-3/* --limit 100 | cut -d" " -f 4 | xargs -L 1 docker pull -a 
+docker search registry.access.redhat.com/jboss-datagrid-6/* --limit 100 | cut -d" " -f 4 | xargs -L 1 docker pull -a
 
 # base images
-docker pull registry.access.redhat.com/rhel7-atomic
-docker pull registry.access.redhat.com/rhel7/rhel
-docker pull registry.access.redhat.com/rhel6/rhel
-docker pull registry.access.redhat.com/rhel7
-docker pull registry.access.redhat.com/rhel6
-docker pull registry.access.redhat.com/rhel
-docker pull registry.access.redhat.com/rhel7.4
-docker pull registry.access.redhat.com/rhel7/rhel-atomic
-docker pull registry.access.redhat.com/rhel-atomic
-docker pull registry.access.redhat.com/rhel6.9
+docker pull registry.access.redhat.com/rhel7-atomic -a
+docker pull registry.access.redhat.com/rhel7/rhel -a
+docker pull registry.access.redhat.com/rhel6/rhel -a
+docker pull registry.access.redhat.com/rhel7 -a
+docker pull registry.access.redhat.com/rhel6 -a
+docker pull registry.access.redhat.com/rhel -a
+docker pull registry.access.redhat.com/rhel7.4 -a
+docker pull registry.access.redhat.com/rhel7/rhel-atomic -a
+docker pull registry.access.redhat.com/rhel-atomic -a
+docker pull registry.access.redhat.com/rhel6.9 -a
 
 docker pull registry.connect.redhat.com/crunchydata/crunchy-pgadmin4
 docker pull registry.connect.redhat.com/crunchydata/crunchy-backup
