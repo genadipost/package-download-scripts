7963f89d3ce97dfc03ae0239e5e45cd3704f46a8
genadipost <genadipost@gmail.com>
2017-10-28 19:02:02 +0300

    clean up

--- docker_notes.sh
+++ docker_notes.sh
@@ -1,90 +1,92 @@
-curl -s https://registry.access.redhat.com/v1/search?q=* | jq '.results[].name' | xargs -L 1 docker pull
-
-curl -s https://registry.access.redhat.com/v1/search?q=* | jq '.results[].name' | xargs -L 1 -I {} "curl -s https://registry.access.redhat.com/v1/repositories/{}/tags"
-
-curl -s https://registry.access.redhat.com/v1/search?q=* | jq '.results[].name' | xargs -L 1 -I {} sh -c  "curl -s https://registry.access.redhat.com/v1/repositories/{}/tags | jq ."
-
-#!/bin/bash
-
-number_of_versions=20
-
-
-images_by_name=`curl -s https://registry.access.redhat.com/v1/search?q=* | jq '.results[].name'`
-for image_name in $images_by_name ; do
-  image_name=`echo "$image_name" | tr -d '"'`
-  echo $image_name
-  images_string=""
-  tags=`curl -s https://registry.access.redhat.com/v1/repositories/$image_name/tags | jq -r 'keys[length-10:length-1]' | jq .[]`
-  for tag in $tags ; do
-     tag=`echo "$tag" | tr -d '"'`
-     echo $image_name:$tag
-     docker pull $image_name:$tag 
-     images_string="$images_string $image_name:$tag"
-  done
-  echo $images_string
-  tar_name=${image_name/\//__}
-  echo "docker save -i $tar_name.tar $images_string"
-  docker save -o /mnt/docker_images/$tar_name.tar $images_string
-  docker rmi $images_string
-done
-
-rhscl/nginx-110-rhel7
-
-curl -s https://registry.access.redhat.com/v1/search?q=* | jq '.results[].name' | xargs -L 1 -I {} sh -c "echo {} | tr -d '\"'" | xargs -L 1 -I {} sh -c "basename {}" | xargs -L 1 -I {} sh -c "echo {};find /mnt/docker_images/ -name *{}*;echo ------"
-
-curl -s https://registry.access.redhat.com/v1/search?q=* | jq '.results[].name' | xargs -L 1 -I {} sh -c "basename {}" | xargs -L 1 -I {} sh -c "echo {};find /mnt/docker_images/ -name *{}*;echo ------"
-
-
+# Dowload images from registry.access.redhat.com
+```
+DOWNLOAD_PATH=/mnt/docker_images/
 images_by_name=`curl -s https://registry.access.redhat.com/v1/search?q=* | jq '.results[].name'`
 for image_name in $images_by_name ; do
   image_name=`echo "$image_name" | tr -d '"'`
   images_string=""
-  tags=`curl -s https://registry.access.redhat.com/v1/repositories/$image_name/tags | jq -r 'keys[length-2:length-1]' | jq .[]`
+  tags=`curl -s https://registry.access.redhat.com/v1/repositories/$image_name/tags | jq -r 'keys[length-8:-1]' | jq .[]`
   tar_name=${image_name/\//__}
-  if [ ! -f /mnt/docker_images/$tar_name.tar ]
+  if [ ! -f $DOWNLOAD_PATH$tar_name.tar ]
     then
      echo $image_name does not exist - download
      for tag in $tags ; do
         tag=`echo "$tag" | tr -d '"'`
         echo $image_name:$tag
-        docker pull $image_name:$tag 
+        docker pull $image_name:$tag
         images_string="$images_string $image_name:$tag"
-   
+
      done
-  
+
      echo $images_string
      echo "docker save -i $tar_name.tar $images_string"
-     docker save -o /mnt/docker_images/$tar_name.tar $images_string
+     docker save -o $DOWNLOAD_PATH$tar_name.tar $images_string
      docker rmi $images_string
   else
     echo $image_name exists
   fi
   echo ------
 done
+```
 
-images_by_name=`curl -s https://registry.access.redhat.com/v1/search?q=* | jq '.results[].name'`
-for image_name in $images_by_name ; do
-  image_name=`echo "$image_name" | tr -d '"'`
+# Download images from docker hub by organization
+```
+#!/bin/bash
+
+# https://gist.github.com/kizbitz/e59f95f7557b4bbb8bf2
+# https://gist.github.com/kizbitz/175be06d0fbbb39bc9bfa6c0cb0d4721
+# Requires 'jq': https://stedolan.github.io/jq/
+
+# set username, password, organization, download path
+RGISTRY_URL=hub.docker.com
+UNAME="user"
+UPASS="password"
+ORG="fabric8"
+DOWNLOAD_PATH=/mnt/docker_images/fabric8/
+
+# -------
+
+set -e
+echo
+
+# get token
+echo "Retrieving token ..."
+TOKEN=$(curl -s -H "Content-Type: application/json" -X POST -d '{"username": "'${UNAME}'", "password": "'${UPASS}'"}' https://${RGISTRY_URL}/v2/users/login/ | jq -r .token)
+
+# get list of repositories
+echo "Retrieving repository list ..."
+REPO_LIST=$(curl -s -H "Authorization: JWT ${TOKEN}" https://${RGISTRY_URL}/v2/repositories/${ORG}/?page_size=100 | jq -r '.results|.[]|.name')
+
+# output images & tags
+echo
+echo "Images and tags for organization: ${ORG}"
+echo
+for i in ${REPO_LIST}
+do
+  echo "${i}"	
+  image_name=`echo "$i" | tr -d '"'`
   images_string=""
-  tags=`curl -s https://registry.access.redhat.com/v1/repositories/$image_name/tags | jq -r 'keys[length-2:length-1]' | jq .[]`
-  tar_name=${image_name/\//__}
-  if [ ! -f /mnt/docker_images/$tar_name.tar ]
-    then
-     echo $image_name does not exist - download
-     for tag in $tags ; do
-        #tag=`echo "$tag" | tr -d '"'`
-        #echo $image_name:$tag
-        #docker pull $image_name:$tag 
-        #images_string="$images_string $image_name:$tag"
-   
-     done
-  
-     echo $images_string
-     echo "docker save -i $tar_name.tar $images_string"
-     #docker save -o /mnt/docker_images/$tar_name.tar $images_string
-     #docker rmi $images_string
+  tar_name=${ORG}__${image_name}
+  if [ ! -f $DOWNLOAD_PATH$tar_name.tar ]
+   then
+    echo $image_name does not exist - download
+    # tags
+    IMAGE_TAGS=$(curl -s -H "Authorization: JWT ${TOKEN}" https://${RGISTRY_URL}/v2/repositories/${ORG}/${i}/tags/?page_size=100 | jq -r '.results|.[0,2]|.name')
+    for j in ${IMAGE_TAGS}
+    do
+      if [ $j != "null" ]
+      then
+         docker pull $ORG/$i:$j
+         images_string="$images_string $ORG/$i:$j"
+      fi
+    done
+    echo
+    echo $images_string
+    echo "docker save -o $DOWNLOAD_PATH$tar_name.tar $images_string"
+    docker save -o $DOWNLOAD_PATH$tar_name.tar $images_string
+    docker rmi $images_string
   else
     echo $image_name exists
   fi
-  echo ------
 done
+```
