fc1619f253d153778a6e5f547292ab2c3b393ab3
genadipost <genadipost@gmail.com>
2017-12-15 22:47:39 +0200

    fix typo

--- rhel7_repos_config.sh
+++ rhel7_repos_config.sh
@@ -175,7 +175,7 @@ enabled=1
 autorefresh=1
 type=rpm-md
 
-echo '[elasticsearch-6.x]
+[elasticsearch-6.x]
 name=Elasticsearch repository for 6.x packages
 baseurl=https://artifacts.elastic.co/packages/6.x/yum
 gpgcheck=1
