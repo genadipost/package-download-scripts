0c72b0a5d57cd7fcc6c28515112b214131d8a278
genadipost <genadipost@gmail.com>
2017-12-15 22:42:49 +0200

    update
    update 3.2.x to 3.2.18
    update 3.4.x to 3.4.10
    add 3.6.x

--- download_mongodb_archive.sh
+++ download_mongodb_archive.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
+# Download mongodb archive for 3.6.x
+for minor_version in {0..0}
+do
+ wget https://downloads.mongodb.com/linux/mongodb-linux-x86_64-enterprise-rhel62-3.6.$minor_version.tgz
+ wget https://downloads.mongodb.com/linux/mongodb-linux-x86_64-enterprise-rhel70-3.6.$minor_version.tgz
+done
+
 # Download mongodb archive for 3.4.x
-for minor_version in {0..9}
+for minor_version in {0..10}
 do
  wget https://downloads.mongodb.com/linux/mongodb-linux-x86_64-enterprise-rhel62-3.4.$minor_version.tgz
  wget https://downloads.mongodb.com/linux/mongodb-linux-x86_64-enterprise-rhel70-3.4.$minor_version.tgz
 done
 
 # Download mongodb archive for 3.2.x
-for minor_version in {0..17}
+for minor_version in {0..18}
 do
  wget https://downloads.mongodb.com/linux/mongodb-linux-x86_64-enterprise-rhel62-3.2.$minor_version.tgz
  wget https://downloads.mongodb.com/linux/mongodb-linux-x86_64-enterprise-rhel70-3.2.$minor_version.tgz
