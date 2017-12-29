7c5f04a794f2b69a9fe5affe61fcee055a34ef42
genadipost <genadipost@gmail.com>
2017-12-30 00:27:12 +0200

    move createrepo into the loop

--- reposync_list.sh
+++ reposync_list.sh
@@ -13,10 +13,9 @@ else
    else
       filelines=`cat $FILENAME`
       for line in $filelines ; do
-         echo ------ syncing $line repository
+         echo ------ syncing $line repository to $DIRECTORY/$line
          reposync -m -l -d --repoid=$line --download_path=$DIRECTORY
+         createrepo --workers 20 -v $DIRECTORY/$line
       done
-
-      for repo in $DIRECTORY/*; do createrepo -v $repo; done;
    fi
 fi
