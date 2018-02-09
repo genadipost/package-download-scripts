2a89ddb101cd6ef41adc4fa3f1131d4a5e1bee94
genadipost <genadipost@gmail.com>
2018-02-09 19:35:08 +0200

    bump versions

--- download_mongodb_archive.sh
+++ download_mongodb_archive.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 # Download mongodb archive for 3.6.x
-for minor_version in {0..1}
+for minor_version in {0..2}
 do
  wget https://downloads.mongodb.com/linux/mongodb-linux-x86_64-enterprise-rhel62-3.6.$minor_version.tgz
  wget https://downloads.mongodb.com/linux/mongodb-linux-x86_64-enterprise-rhel70-3.6.$minor_version.tgz
 done
 
 # Download mongodb archive for 3.4.x
-for minor_version in {0..10}
+for minor_version in {0..12}
 do
  wget https://downloads.mongodb.com/linux/mongodb-linux-x86_64-enterprise-rhel62-3.4.$minor_version.tgz
  wget https://downloads.mongodb.com/linux/mongodb-linux-x86_64-enterprise-rhel70-3.4.$minor_version.tgz
 done
 
 # Download mongodb archive for 3.2.x
-for minor_version in {0..18}
+for minor_version in {0..19}
 do
  wget https://downloads.mongodb.com/linux/mongodb-linux-x86_64-enterprise-rhel62-3.2.$minor_version.tgz
  wget https://downloads.mongodb.com/linux/mongodb-linux-x86_64-enterprise-rhel70-3.2.$minor_version.tgz
