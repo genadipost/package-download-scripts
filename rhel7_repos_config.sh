ed311047ce944f518e8a5ff280d02794b0902f12
genadipost <genadipost@gmail.com>
2018-08-01 00:31:26 +0300

    Add ansible-2.6 yum repo for rhel 7

--- rhel7_repos_config.sh
+++ rhel7_repos_config.sh
@@ -163,7 +163,8 @@ subscription-manager repos \
     --enable=rhel-sjis-for-rhel-7-server-eus-rpms \
     --enable=rhel-7-server-rhscon-2-installer-rpms \
     --enable=rhel-7-server-openstack-6.0-installer-rpms \
-    --enable=rhel-7-server-ose-3.10-rpms
+    --enable=rhel-7-server-ose-3.10-rpms \
+    --enable=rhel-7-server-ansible-2.6-rpms
 	
 yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 yum install -y https://centos7.iuscommunity.org/ius-release.rpm
